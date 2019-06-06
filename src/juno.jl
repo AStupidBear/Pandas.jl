36b3fc3649008e9a0ffcd42d9dcafa3a605fc609
Yao Lu <luyaocns@gmail.com>
2019-06-07 00:36:12 +0800

    open JTable

--- src/juno.jl
+++ src/juno.jl
@@ -9,11 +9,15 @@ end
 JTable(x::AbstractArray) = JTable(DataFrame(x))
 
 function Base.show(io::IO, ::MIME"application/prs.juno.plotpane+html", tbl::JTable)
-    html = tempname() * ".html"
-    csv = joinpath(homedir(), ".cache/jtable.csv")
     df = round(tbl.data, 3)
+    html = tempname() * ".html"
     write_html_table(html, df)
+    print(io, read(html, String))
+end
+
+function Base.open(tbl::JTable)
+    df = round(tbl.data, 3)
+    csv = joinpath(homedir(), ".cache/jtable.csv")
     to_csv(df, csv)
     Atom.msg("openFile", csv)
-    print(io, read(html, String))
 end
